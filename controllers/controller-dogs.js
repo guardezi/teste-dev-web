055e870fdd4fa5053fb44301a855acc407bb0af2
Guardezi <jefersonguardezi@gmail.com>
2015-08-09 16:38:39 -0300

    envio da imagem feito pelo angular

--- controllers/controller-dogs.js
+++ controllers/controller-dogs.js
@@ -3,15 +3,8 @@
  */
 
 var mongoDbConnection = require('./connection');
-var cloudinary = require('cloudinary');
 
-var fs = require('fs');
 
-cloudinary.config({
-    cloud_name: 'hwr0nwo83',
-    api_key: '129885417761768',
-    api_secret: 'xb909yce4NHq9Ix3s5dawISIDYY'
-});
 
 var dogs = new Object();
 
@@ -45,18 +38,12 @@ dogs.getByBreed = function(name, callback){
 
 
 dogs.save = function(dog,callback){
-    var imgPath = '../public/images/dog.jpg';
-    cloudinary.uploader.upload(imgPath, function(result) {
-
-        dog.img_url = result.url;
-
-        mongoDbConnection(function(databaseConnection) {
-            databaseConnection.collection('dog', function (error, collection) {
-                collection.insert(dog);
-                if (error)throw  new error(error);
-                callback(dog);
-                return;
-            });
+    mongoDbConnection(function(databaseConnection) {
+        databaseConnection.collection('dog', function (error, collection) {
+            collection.insert(dog);
+            if (error)throw  new error(error);
+            callback(dog);
+            return;
         });
     });
 };
