7980c4c382ecb10c1b75f58941a840f8277f6d35
Guardezi <jefersonguardezi@gmail.com>
2015-08-10 22:03:15 -0300

    ajuste

--- public/app/dog/dog.js
+++ public/app/dog/dog.js
@@ -11,8 +11,7 @@ angular.module('myApp.dog', ['ngRoute'])
 
 .controller('dogCtrl', ['$scope','$http','$routeParams',function($scope,$http,$routeParams) {
     var buscar_cachorros_porID = function () {
-//         $http.get('https://charliedogs.herokuapp.com/dogs/id/'+$routeParams.id)
-        $http.get('http://127.0.0.1:5000/dogs/id/'+$routeParams.id)
+         $http.get('https://charliedogs.herokuapp.com/dogs/id/'+$routeParams.id)
             .success(function (data, status, headers, config) {
                 $scope.dog = data;
                 console.log($scope.dog);
